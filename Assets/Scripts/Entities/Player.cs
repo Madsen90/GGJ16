7bcd3722038d69962094eec97caec1611ebe58f0
Madsen90 <frederikmusen@gmail.com>
2016-01-29 23:06:36 +0100

    pushable

---
+++ Assets/Scripts/Entities/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float Speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 move = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            move += Vector3.left;
+        if (Input.GetKey(KeyCode.RightArrow))
+            move += Vector3.right;
+        if (Input.GetKey(KeyCode.UpArrow))
+            move += Vector3.up;
+        if (Input.GetKey(KeyCode.DownArrow))
+            move += Vector3.down;
+        //if(move != Vector3.zero)
+        //{
+        //    Debug.Log("player position:" + transform.position);
+
+        //}
+        transform.position += move * Speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Entities/Pushable.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pushable : MonoBehaviour {
+    private Vector3 initPos;
+    public float MaxPush;
+
+	// Use this for initialization
+	void Start () {
+        initPos = transform.position;
+        Debug.Log(initPos);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerStay2D(Collider2D player)
+    {
+        Vector3 delta = initPos - player.transform.position;
+        float distance = delta.magnitude;
+
+        if (distance == 0)
+        {
+            return;
+        }
+
+        if (distance > MaxPush)
+        {
+            transform.position = initPos;
+            return;
+        }
+
+        Vector3 direction = delta / distance;
+        float distance2 = MaxPush - distance;
+
+        transform.position = initPos + direction * distance2;
+
+
+        //if (transform.position == initPos)
+        //{
+        //    Debug.Log("delta:" + delta);
+        //    Debug.Log("dis:" + distance);
+        //    Debug.Log("dir:" + direction);
+        //    Debug.Log("dis2:" + distance2);
+        //}
+    }
+
+    void OnTriggerExit2D(Collider2D player)
+    {
+       // transform.position = initPos;
+    }
+}
