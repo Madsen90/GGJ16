89957366aa10e80021d15acecf73149ee0950792
kirkbro <maek@itu.dk>
2016-01-31 01:45:34 +0100

    sound/anim

--- Assets/Scripts/Entities/SetNoise.cs
+++ Assets/Scripts/Entities/SetNoise.cs
@@ -1,12 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+
+using UnityEngine;
 using System.Collections;
 
 public class SetNoise : MonoBehaviour
 {
 	public float VolumeTarget;
+    public float FadeDuration = 2;
 
     private AudioSource ritual;
     private AudioSource noise;
+
+    private float timeLeft;
+    private float startVolume;
 
     void Start ()
     {
@@ -15,9 +21,25 @@ public class SetNoise : MonoBehaviour
         noise = sources[1];
     }
 
+    void Update()
+    {
+        if (timeLeft > 0)
+        {
+            var progress = (FadeDuration - timeLeft) / FadeDuration; //0,1
+            var delta = VolumeTarget - startVolume; //1-0 = 1
+            noise.volume = startVolume + delta * progress; // 0+1*0,1 = 0,1
+
+            Debug.Log(String.Format("progress: {0}, delta: {1}, volume: {2}", progress, delta, noise.volume));
+
+            timeLeft -= Time.deltaTime;
+        }
+    }
+
     void OnTriggerEnter2D()
     {
-        noise.volume = Mathf.Max(noise.volume, VolumeTarget);
-        ritual.volume = 1 - noise.volume;
+        if (noise.volume > VolumeTarget) return;
+        timeLeft = FadeDuration;
+        startVolume = noise.volume;
+        Debug.Log(String.Format("FadeDuration: {0}, startVolume: {1}", FadeDuration, startVolume));
     }
 }
